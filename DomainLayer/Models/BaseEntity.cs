b2a248861aa7864718dc285b387e3f1698b1fb3b
Trupti Khade <trupti.khade@tietoevry.com>
2024-05-22 13:45:56 +0530

    Added 4.7.2

--- DomainLayer/Models/BaseEntity.cs
+++ DomainLayer/Models/BaseEntity.cs
@@ -12,20 +12,5 @@ namespace DomainLayer.Models
             get;
             set;
         }
-        //public DateTime CreatedDate
-        //{
-        //    get;
-        //    set;
-        //}
-        //public DateTime ModifiedDate
-        //{
-        //    get;
-        //    set;
-        //}
-        //public bool IsActive
-        //{
-        //    get;
-        //    set;
-        //}
     }
 }
